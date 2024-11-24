8ca5c5655a2a026da9594224f78e11b99262ba82
dntlakfn <83486067+dntlakfn@users.noreply.github.com>
2024-11-24 23:58:31 +0900

    feat : WaveBtn

--- Assets/01_Works/dntlakfn/WaveBtn.cs
+++ Assets/01_Works/dntlakfn/WaveBtn.cs
@@ -8,6 +8,13 @@ public class WaveBtn : MonoBehaviour
     private void OnEnable()
     {
         transform.localPosition = new Vector3(transform.localPosition.x, -700);
-        transform.DOLocalMoveY(-350, 1);
+        transform.DOLocalMoveY(-350, 1).SetEase(Ease.InBack);
+    }
+
+
+    public void Down()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, -350);
+        transform.DOLocalMoveY(-700, 1).SetEase(Ease.InBack);
     }
 }
