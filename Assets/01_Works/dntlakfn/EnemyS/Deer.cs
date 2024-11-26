7af789a3155aa0e8f4591dc8c7c26d4827d1a25a
dntlakfn <83486067+dntlakfn@users.noreply.github.com>
2024-11-26 11:00:25 +0900

    fix : Enemys

--- Assets/01_Works/dntlakfn/EnemyS/Deer.cs
+++ Assets/01_Works/dntlakfn/EnemyS/Deer.cs
@@ -1,21 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Deer : Enemy
 {
-    // 자기보다 앞으로 가지못함(새 제외), 자신보다 앞에 가까이있는 적들을 빨라지게함 , 처음으로 맞기 전까지 이동속도가 빨라짐
+    
     bool isDamage;
     protected override void UniqueSkill()
     {
         animator.speed = 2;
-        speed = 2f;
+        speed = 1.5f;
 
     }
 
-    private void Start()
+    public override void OnEnable()
     {
+        base.OnEnable();
         UniqueSkill();
         isDamage = false;
     }
