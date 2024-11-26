231f9413f9a0db4336b503c72e2d1b7304f8f2ca
plmknbuhv <plmknbuhv@gmail.com>
2024-11-26 23:41:39 +0900

    .

--- Assets/01_Works/dntlakfn/FirstCheck.cs
+++ Assets/01_Works/dntlakfn/FirstCheck.cs
@@ -1,18 +1,32 @@
 using System.IO;
+using System.Text;
 using UnityEngine;
 
 public class FirstCheck : MonoBehaviour
 {
     public bool CheckIsFirstPlay()
     {
+        // Unity 권장 경로로 파일 저장
+        string filePath = Path.Combine(Application.persistentDataPath, "fc.txt");
+
         try
         {
-            return bool.Parse(File.ReadAllText(@"C:\fc.txt"));
+            // 파일 읽기
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            using (StreamReader reader = new StreamReader(fs, Encoding.UTF8))
+            {
+                return bool.Parse(reader.ReadToEnd());
+            }
         }
         catch
         {
-            File.WriteAllText(@"C:\fc.txt", "false");
+            // 파일 생성 및 쓰기
+            using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            using (StreamWriter writer = new StreamWriter(fs, Encoding.UTF8))
+            {
+                writer.Write("false");
+            }
             return true;
-        }   
+        }
     }
 }
